f86752896882b62b33e8ec202c355f3e44a20a40
lakshmydivyasharma <lakshmydivyasharma@users.noreply.github.com>
2020-03-16 09:24:03 -0400

    node palindrome

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('#button').addEventListener('click', makeReq)
+const ul = document.querySelector('ul')
+function makeReq(){
+
+  const userName = document.querySelector("#name").value;
+
+  fetch(`/palindrome?checkPalindrome=${userName}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      input.textContent = data.message
+    });
+
+}
